d4747d63f53e72b931bc1dae3cf1159569d62481
STM <legioner9>
2019-11-10 19:13:48 +0700

    init 3

--- nodejs.org_DOCS/Stream/Fs_Streams/read_gzip_write.js
+++ nodejs.org_DOCS/Stream/Fs_Streams/read_gzip_write.js
@@ -3,5 +3,5 @@ const zlib = require('zlib');
 const gzip = zlib.createGzip();
 const outStream = fs.createWriteStream('output.js.gz');
 fs.createReadStream(__filename)
-    .pipe(gzip)
+    // .pipe(gzip)
     .pipe(outStream);
