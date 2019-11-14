83193016891b5d55781fe6a340a075b50596ba05
STM <legioner9>
2019-11-14 17:35:02 +0700

    init v.4

---
+++ nodejs.org_DOCS/Util/util.callbackify.js
@@ -0,0 +1,25 @@
+const util = require('util');
+const fs = require('fs');
+
+const pf = new Promise(((resolve, reject) => {
+    setTimeout(() => {
+        resolve('YES');
+    }, 100);
+    setTimeout(() => {
+        reject('NO');
+    }, 200);
+}));
+
+// pf.then(resolve => console.log('resolve::', resolve),
+//     reject => console.log('reject::', reject));
+
+function ret_pf() {
+    return pf;
+}
+
+const cb_pf = util.callbackify(ret_pf);
+
+cb_pf((e, d) => {
+    if (e) throw e;
+    console.log('d::', d);
+});
