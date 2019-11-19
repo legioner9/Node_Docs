f5cd8ba46448b5427dd87c778db1c52d35f116f9
STM <legioner9>
2019-11-19 12:32:49 +0700

    #TSH p.5 Project +lib.js +index.js zip v.1

--- Education/Timur_Video_Node.js/part_5/Project-master/Project-master/cache.js
+++ Education/Timur_Video_Node.js/part_5/Project-master/Project-master/cache.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const lib1 = require('./main.js');
-const libPath = require.resolve('./main.js');
+const lib1 = require('./lib/lib.js');
+const libPath = require.resolve('./lib.js');
 console.dir({ libPath });
 delete require.cache[libPath];
-const lib2 = require('./main.js');
+const lib2 = require('./lib/lib.js');
 
 if (lib1 === lib2) {
   console.log('Cache not reloaded');
