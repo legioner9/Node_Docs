8fcb2ae5d39f96f5988a843d5602bc6929686b3e
STM <legioner9>
2019-11-22 15:47:13 +0700

    #TSH p.8 autoreload v.3

--- nodejs.org_DOCS/Fs/Watch/autoreload.js
+++ nodejs.org_DOCS/Fs/Watch/autoreload.js
@@ -5,27 +5,36 @@ const load = path_from => {
     fs.readFile(path_from, 'utf-8', (e, d) => {
         if (e) throw e;
         const path_to = path.join(__dirname, 'Copy', file_name);
-        console.log(`Buf = ${d}`);
-        console.log(`Read from ${path_from}`);
-        console.log(`Write to ${path_to}`);
+        // console.log(`Buf = ${d}`);
+        // console.log(`Read from ${path_from}`);
+        // console.log(`Write to ${path_to}`);
         write(path_to, d);
     })
 };
 const write = (path, buf) => {
     fs.writeFile(path, buf, e => {
         if (e) throw e;
-        console.log(`Write to ${path}`);
+        console.log(`Write to ${path} \n ${buf}`);
     })
 };
 
 const watch = filename => {
     const path_watch = path.join(__dirname, filename);
+    console.log('\x1Bc'); // ESC sequence  but not here(((
     fs.watch(path_watch, (event, filename) => {
-        console.log({event, filename});
-        load(path_watch);
+        if (event === 'change') {
+            console.log({event, filename});
+            load(path_watch)
+        }
     })
 };
 
 const file_name = 'toCopy.txt';
-watch(file_name);
 
+try {
+    watch(file_name);
+} catch (e) {
+    console.error(e.message);
+} finally {
+    watch(file_name);
+}
