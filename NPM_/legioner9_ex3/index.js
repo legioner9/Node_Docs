26cb5b21588d653a701921b1e9b2b7a7602cb0f6
STM <legioner9>
2019-12-24 00:18:09 +0700

    init legioner9_ex3 v.1

--- NPM_/legioner9_ex3/index.js
+++ NPM_/legioner9_ex3/index.js
@@ -1,10 +1,12 @@
 const my_path = require ( 'path' );
 const ServCacheModulesOfMethods = require ( './Ingx/ServCacheModulesOfMethods/I_getCache_startServ.js' );
-
+const SuLog = require ( './Ingx/Loggers/Su_log_to_file.js' );
 const { SuServer, SuHandServ } = require ( my_path.join ( __dirname, 'Ingx', 'Servers' ) );
 
 module.exports = {
     SuServer,
     SuHandServ,
     ServCacheModulesOfMethods,
-};
+    SuLog,
+}
+;
