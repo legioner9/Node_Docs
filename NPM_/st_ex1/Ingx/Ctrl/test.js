1b23f039b79200ca1d7c7b58cc261b7a416bd3fb
STM <legioner9>
2019-12-24 22:07:29 +0700

    init st_ex1 st_doc1 v.1

---
+++ NPM_/st_ex1/Ingx/Ctrl/test.js
@@ -0,0 +1,10 @@
+const my_path = require ( 'path' );
+const path = my_path.join ( __dirname, '../index.js' );
+const {
+    SuServer,
+    SuHandServ,
+    ServCacheModulesOfMethods,
+    Su_log_to_file,
+    SuTry,
+} = require ( my_path.join ( __dirname, '../../index.js' ) );
+debugger;
