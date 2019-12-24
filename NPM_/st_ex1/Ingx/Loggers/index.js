2f43be90e7c0ec90e8f87b8b03fdc1256fdecad1
STM <legioner9>
2019-12-25 03:15:27 +0700

    Assert SuTry Box v.

--- NPM_/st_ex1/Ingx/Loggers/index.js
+++ NPM_/st_ex1/Ingx/Loggers/index.js
@@ -1,21 +1,24 @@
+// v1.0.4;
+
 const { Readable, Writable, Transform, pipeline } = require ( 'stream' );
 const fs = require ( 'fs' );
 const my_path = require ( 'path' );
 
 function Su_log_to_file ( str, file_name = 'SuLog.txt', dir_name = 'SuLog' ) {
 
-    fs.mkdir ( my_path.join ( __dirname, dir_name ), { recursive: true }, ( err ) => {
+    fs.mkdir ( my_path.join ( dir_name ), { recursive: true }, ( err ) => {
         if ( err ) throw err;
-        s_log_to_file ( str, file_name, dir_name );
+        s_log_to_file ( str, my_path.join ( dir_name, file_name ) );
     } );
 }
 
-function s_log_to_file ( str, file_name, dir_name ) {
+function s_log_to_file ( str, path ) {
+
     const rstr = new Readable ();
     rstr.push ( str );
     rstr.push ( null );
     // inStream.pipe ( process.stdout );
-    const write_fs = fs.createWriteStream ( my_path.join ( __dirname, dir_name, file_name ), 'utf-8' );
+    const write_fs = fs.createWriteStream ( path, 'utf-8' );
     write_fs.on ( 'error', ( e ) => console.log ( 'WTF?(((', e ) );
     pipeline (
         rstr,
@@ -43,7 +46,8 @@ function s_log_to_file ( str, file_name, dir_name ) {
     );
 }
 
-const SuTry = ( fn ) => ( ...args ) => {
+const SuTry = ( fn ) => ( file, dir, ...args ) => {
+
     try {
         fn ( ...args );
     }
@@ -52,10 +56,11 @@ const SuTry = ( fn ) => ( ...args ) => {
     ${ e.stack }
     
     ${ new Date () }`;
-        Su_log_to_file ( str );
+        Su_log_to_file ( str, file, dir );
     }
 };
 
+// SuTry ( assert.strictEqual ) ( 'test_log.txt', my_path.join ( __dirname, 'TestLog' ), 3, 5 );
 module.exports = {
     Su_log_to_file,
     SuTry,
