26eac2d32df7aa145c342805beffd6529bf66407
STM <legioner9>
2019-12-31 16:58:36 +0700

    node_modules st_doc1 v1.0.5 Buffer Doc 6.js v.3

--- NPM_/ST_DOC/st_doc1~1.0.5/nodejs.org_DOCS/Buffer/DOC/Coverage/9#1.js
+++ NPM_/ST_DOC/st_doc1~1.0.5/nodejs.org_DOCS/Buffer/DOC/Coverage/9#1.js
@@ -1,8 +1,13 @@
 const assert = require ( 'assert' );
-
+const Ex = require ( 'st_ex1' );
+// Ex.call ();
+const su_try = Ex.SetTry.TryCatch;
+console.log ( su_try.help );
 const buf1 = Buffer.from ( 'this is a tést' );
 const buf2 = Buffer.from ( '7468697320697320612074c3a97374', 'hex' );
-assert.strictEqual();
+
+su_try ( assert.deepStrictEqual ) ( '#to_consJson', null, buf1, buf2 );
+debugger;
 
 const convert_str_utf8_to_hex = Buffer.from ( 'this is a tést' ).toString ( 'hex' );
 //
